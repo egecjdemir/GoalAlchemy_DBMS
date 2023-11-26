656f62881b261249ca1ad646ee5d5f7e9ba41337
Ege Demir <118561286+egecjdemir@users.noreply.github.com>
2023-11-26 18:04:12 +0300

    function to import csv file in MySQL table

---
+++ csv_to_sql_table.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import Error
+import configparser
+import csv
+
+config = configparser.ConfigParser()
+config.read('config.py')
+
+user = config.get('database', 'user')
+password = config.get('database', 'password')
+host = config.get('database', 'host')
+database = config.get('database', 'database')
+
+try:
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    cursor = connection.cursor()
+
+    def csv_to_sql_table(file_path, table_name):
+        with open(csv_file_path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+
+            header = next(reader)
+            num_columns = len(header)
+
+            placeholders = ', '.join(['%s'] * num_columns)
+
+            for row in reader:
+                cursor.execute(f"INSERT INTO {table_name} VALUES ({placeholders})", tuple(row))
+
+        connection.commit()
+
+    csv_to_sql_table('data/clubs.csv', 'clubs')
+    csv_to_sql_table('data/games.csv', 'games')
+
+except Error as e:
+    print(f'Error: {e}')
+
+finally:
+    cursor.close()
+    connection.close()
