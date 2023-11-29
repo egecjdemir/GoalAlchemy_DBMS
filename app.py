f0eba1d891637af82f533512880654ecdd209158
itu-itis22-demirz20 <demirz20@itu.edu.tr>
2023-11-29 17:27:45 +0300

    Created data_cleaning.ipynb and added some cleaned version of tables. Also fixed some mistakes in app.py and csv_to_sql_table.py.
    I changed the creation order of tables, fixed some foreign key mistakes, forgotten commas, and wrong data types. I changed club_games table's primary key. Also changed the content of csv_to_sql_table to handle Null values and changed the path of csv files. I created data_cleaning.ipynb to drop some unmatched rows and I writed new data to cleaned csv files.

--- app.py
+++ app.py
@@ -20,25 +20,68 @@ try:
 
     cursor = connection.cursor()
 
-    create_table_queries = '''
-        CREATE TABLE appearances (
-        appearance_id INT PRIMARY KEY,
-        game_id INT NOT NULL,
-        player_id INT NOT NULL,
-        player_club_id INT,
-        player_current_club_id INT,
-        date DATE,
-        player_name VARCHAR(255),
-        competition_id INT,
-        yellow_cards INT,
-        red_cards INT,
-        goals INT,
-        assists INT,
-        minutes_played INT,  
-        FOREIGN KEY (player_id) REFERENCES players(player_id),
-        FOREIGN KEY (game_id) REFERENCES games(game_id));
-        
-        CREATE TABLE players (
+    create_table_queries = '''        
+    CREATE TABLE competitions (
+        competition_id VARCHAR(50) PRIMARY KEY,
+        competition_code VARCHAR(50) NOT NULL,
+        name VARCHAR(255),
+        sub_type VARCHAR(50),
+        type VARCHAR(50),
+        country_id INT,
+        country_name VARCHAR(50),
+        domestic_league_code VARCHAR(50),
+        confederation VARCHAR(50),
+        url VARCHAR(255)
+    );
+    
+    CREATE TABLE clubs (
+        club_id INT PRIMARY KEY, 
+        club_code VARCHAR(50), 
+        name VARCHAR(50), 
+        domestic_competition_id VARCHAR(50),
+        total_market_value VARCHAR(50), 
+        squad_size INT, 
+        average_age DECIMAL, 
+        foreigners_number INT,
+        foreigners_percentage DECIMAL, 
+        national_team_players INT, 
+        stadium_name VARCHAR(50),
+        stadium_seats INT, 
+        net_transfer_record VARCHAR(50), 
+        coach_name VARCHAR(50), 
+        last_season INT,
+        url VARCHAR(255),
+        FOREIGN KEY (domestic_competition_id) REFERENCES competitions(competition_id)
+    );
+    
+    CREATE TABLE games (
+        game_id INT PRIMARY KEY, 
+        competition_id VARCHAR(50), 
+        season INT, 
+        round VARCHAR(50), 
+        date DATE, 
+        home_club_id INT,
+        away_club_id INT, 
+        home_club_goals INT, 
+        away_club_goals INT,
+        home_club_position INT, 
+        away_club_position INT, 
+        home_club_manager_name VARCHAR(50),
+        away_club_manager_name VARCHAR(50), 
+        stadium VARCHAR(50), 
+        attendance INT, 
+        referee VARCHAR(50), 
+        url VARCHAR(255),
+        home_club_name VARCHAR(50), 
+        away_club_name VARCHAR(50), 
+        aggregate VARCHAR(50), 
+        competition_type VARCHAR(50),
+        FOREIGN KEY (competition_id) REFERENCES competitions(competition_id),
+        FOREIGN KEY (home_club_id) REFERENCES clubs(club_id),
+        FOREIGN KEY (away_club_id) REFERENCES clubs(club_id)
+    );
+    
+    CREATE TABLE players (
         player_id INT PRIMARY KEY,
         first_name VARCHAR(255),
         last_name VARCHAR(255),
@@ -60,13 +103,33 @@ try:
         agent_name VARCHAR(255),
         image_url VARCHAR(255),
         url VARCHAR(255),
-        current_club_domestic_competition_id INT,
+        current_club_domestic_competition_id  VARCHAR(50),
         current_club_name VARCHAR(50),
         FOREIGN KEY (current_club_id) REFERENCES clubs(club_id)
-        );
-        
-        CREATE TABLE club_games (
-        game_id INT PRIMARY KEY,
+    );
+    
+    
+    CREATE TABLE appearances (
+        appearance_id VARCHAR(50) PRIMARY KEY,
+        game_id INT,
+        player_id INT,
+        player_club_id INT,
+        player_current_club_id INT,
+        date DATE,
+        player_name VARCHAR(255),
+        competition_id VARCHAR(50),
+        yellow_cards INT,
+        red_cards INT,
+        goals INT,
+        assists INT,
+        minutes_played INT,  
+        FOREIGN KEY (player_id) REFERENCES players(player_id),
+        FOREIGN KEY (game_id) REFERENCES games(game_id)
+    );
+    
+    
+    CREATE TABLE club_games (
+        game_id INT NOT NULL,
         club_id INT NOT NULL,
         own_goals INT,
         own_position INT,
@@ -77,78 +140,20 @@ try:
         opponent_manager_name VARCHAR(255),
         hosting VARCHAR(50),
         is_win BOOLEAN,
-        FOREIGN KEY (opponent_id) REFERENCES games(club_id),
-        FOREIGN KEY (club_id) REFERENCES clubs(club_id)
-        );
-        
-        CREATE TABLE competitions (
-        competition_id VARCHAR(50) PRIMARY KEY,
-        competition_code VARCHAR(50) NOT NULL,
-        name VARCHAR(255),
-        sub_type VARCHAR(50),
-        type VARCHAR(50),
-        country_id INT,
-        country_name VARCHAR(50),
-        domestic_league_code VARCHAR(50),
-        confederation VARCHAR(50),
-        url VARCHAR(255)
-        )
+        PRIMARY KEY (club_id, game_id),
+        FOREIGN KEY (game_id) REFERENCES games(game_id),
+        FOREIGN KEY (club_id) REFERENCES clubs(club_id),
+        FOREIGN KEY (opponent_id) REFERENCES clubs(club_id)
+    );
 
-        CREATE TABLE games (
-            game_id INT PRIMARY KEY, 
-
-            competition_id VARCHAR(50) NOT NULL, 
-            season INT, 
-            round VARCHAR(50), 
-            date DATE, 
-            home_club_id INT NOT NULL,
-            away_club_id INT NOT NULL, 
-            home_club_goals INT, 
-            away_club_goals INT,
-            home_club_position INT, 
-            away_club_position INT, 
-            home_club_manager_name VARCHAR(50),
-            away_club_manager_name VARCHAR(50), 
-            stadium VARCHAR(50), 
-            attendance INT, 
-            referee VARCHAR(50), 
-            url VARCHAR(255),
-            home_club_name VARCHAR(50), 
-            away_club_name VARCHAR(50), 
-            aggregate VARCHAR(50), 
-            competition_type VARCHAR(50)
-
-            FOREIGN KEY (competition_id) REFERENCES competitions(competition_id),
-            FOREIGN KEY (home_club_id) REFERENCES clubs(club_id)
-            FOREIGN KEY (away_club_id) REFERENCES clubs(club_id)
-        );
-
-        CREATE TABLE clubs (
-            club_id INT PRIMARY KEY, 
-
-            club_code VARCHAR(50), 
-            name VARCHAR(50), 
-            domestic_competition_id INT NOT NULL,
-            total_market_value VARCHAR(50), 
-            squad_size INT, 
-            average_age DECIMAL(3,1), 
-            foreigners_number INT,
-            foreigners_percentage DECIMAL(3,1), 
-            national_team_players INT, 
-            stadium_name VARCHAR(50),
-            stadium_seats INT, 
-            net_transfer_record VARCHAR(50), 
-            coach_name VARCHAR(50), 
-            last_season INT,
-            url VARCHAR(255)
-
-            FOREIGN KEY (domestic_competition_id) REFERENCES competitions(competition_id),
-        );
     '''
 
     cursor.execute(create_table_queries)
-    print('Tables created.')
-
+    
+    connection.commit()
+    
+    
+    
 except Error as e:
     print(f'Error: {e}')
 
--- csv_to_sql_table.py
+++ csv_to_sql_table.py
@@ -21,26 +21,35 @@ try:
 
     cursor = connection.cursor()
 
-    def csv_to_sql_table(file_path, table_name):
-        with open(csv_file_path, 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
+    def csv_to_sql_table(csv_file, table_name):
+        try:
+            with open(csv_file, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
 
-            header = next(reader)
-            num_columns = len(header)
+                header = next(reader)  # Skip the header row
+                num_columns = len(header)
 
-            placeholders = ', '.join(['%s'] * num_columns)
+                # Adjust placeholders based on the presence of 'null'
+                placeholders = ', '.join(['%s' if 'null' not in value.lower() else 'NULL' for value in header])
+                query = f"INSERT INTO {table_name} VALUES ({placeholders})"
 
-            for row in reader:
-                cursor.execute(f"INSERT INTO {table_name} VALUES ({placeholders})", tuple(row))
+                for row in reader:
+                    # Convert empty values to actual Python None values
+                    row = [None if value.strip() == '' or 'null' in value.lower() else value for value in row]
+                    cursor.execute(query, tuple(row))
 
-        connection.commit()
+                connection.commit()
 
-    csv_to_sql_table('data/clubs.csv', 'clubs')
-    csv_to_sql_table('data/games.csv', 'games')
-    csv_to_sql_table('data/players.csv', 'players')
-    csv_to_sql_table('data/appearances.csv', 'appearances')
-    csv_to_sql_table('data/competitions.csv', 'competitions')
-    csv_to_sql_table('data/club_games.csv', 'club games')
+        except Error as e:
+            print(f"Error: {e}")
+
+        
+    csv_to_sql_table('../data/competitions.csv', 'competitions')
+    csv_to_sql_table('../data/clubs.csv', 'clubs')
+    csv_to_sql_table('../data/players.csv', 'players')
+    csv_to_sql_table('../data/games_cleaned.csv', 'games')
+    csv_to_sql_table('../data/appearances_cleaned.csv', 'appearances') 
+    csv_to_sql_table('../data/club_games_cleaned.csv', 'club_games')
     
 except Error as e:
     print(f'Error: {e}')
